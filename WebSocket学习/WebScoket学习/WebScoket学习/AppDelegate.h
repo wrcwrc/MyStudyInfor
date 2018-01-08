688935062da7fb4f8da3f45dfbda9309808fed45
韦荣炽 <weirongchi@weirongchideMacBook-Pro.local>
2018-01-08 17:58:23 +0800

    第一次添加

---
+++ WebSocket学习/WebScoket学习/WebScoket学习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebScoket学习
+//
+//  Created by 韦荣炽 on 2018/1/5.
+//  Copyright © 2018年 XingFei_韦荣炽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebSocket学习/WebScoket学习/WebScoket学习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebScoket学习
+//
+//  Created by 韦荣炽 on 2018/1/5.
+//  Copyright © 2018年 XingFei_韦荣炽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
